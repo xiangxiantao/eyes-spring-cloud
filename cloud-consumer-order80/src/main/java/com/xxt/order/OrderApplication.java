0d6dd313649959666612f9af1b08644d5fdfa61f
xiantao.xiang <xiantao.xiang@dmall.com>
2022-02-19 18:04:21 +0800

    feat(eureka): order服务注册到eureka

--- cloud-consumer-order80/src/main/java/com/xxt/order/OrderApplication.java
+++ cloud-consumer-order80/src/main/java/com/xxt/order/OrderApplication.java
@@ -2,12 +2,14 @@ package com.xxt.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author xiantao.xiang
  * @date 2022-02-18 22:28
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
