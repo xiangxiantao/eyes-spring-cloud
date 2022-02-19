78e0ca0cd03f0a6d3737434186a3d2ff60f6eb5c
xiantao.xiang <xiantao.xiang@dmall.com>
2022-02-19 18:26:45 +0800

    feat(eureka): 搭建eureka服务集群

---
+++ cloud-eureka-server7002/src/main/java/com/xxt/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.xxt.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author xiantao.xiang
+ * @date 2022-02-19 16:30
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
